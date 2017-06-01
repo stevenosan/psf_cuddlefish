e0ea8cd529bc7b3bbfe8500930a4f1f8c87b38fe
Jorje Nava <jnava@cardinalsolutions.com>
2017-06-01 09:27:06 -0400

    Added gallery link and controller

---
+++ PSF_Cuddlefish/src/PSF_Cuddlefish/Controllers/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PSF_Cuddlefish.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
